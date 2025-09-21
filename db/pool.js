702ad01842d8767cbee5d7deacbf14ca5775f91c
petrudemian <petredemi@gmail.com>
2025-09-21 17:02:36 +0100

    added conections details koyeb

--- db/pool.js
+++ db/pool.js
@@ -1,11 +1,11 @@
 const {Pool} = require("pg")
-module.exports = new Pool({
-  host: "localhost", // or wherever the db is hosted
-  user: "petrudemian",
-  database: "inventory_app",
-  password: "<role_password>",
-  port: 5432 // The default port
-});
+//module.exports = new Pool({
+//  host: "localhost", // or wherever the db is hosted
+//  user: "petrudemian",
+//  database: "inventory_app",
+//  password: "<role_password>",
+//  port: 5432 // The default port
+//});
 //module.exports = new Pool({
 //  host:"dpg-d2urvdvdiees739af9ag-a",
 //  user: "petru",
@@ -13,4 +13,10 @@ module.exports = new Pool({
 //  password: "ZAkk8d0zUdEusUDuHRKvEiPVcg44cwxq",
 //  port: 5432 // The default port
 //});
-
+module.exports = new Pool({
+  host: "ep-little-base-a2wm2x79.eu-central-1.pg.koyeb.app", // or wherever the db is hosted
+  user: "koyeb-adm",
+  database: "inventoryapp",
+  password: "npg_djYLHq3UVa2T",
+  port: 5432 // The default port
+});
